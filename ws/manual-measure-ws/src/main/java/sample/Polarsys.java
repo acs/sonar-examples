b85db0d70f32ac1b6c62dfa4eff75fb4c0165b93
Alvaro del Castillo <acs@bitergia.com>
2014-10-29 08:16:17 +0100

    Added dit and public_api metrics.

--- ws/manual-measure-ws/src/main/java/sample/Polarsys.java
+++ ws/manual-measure-ws/src/main/java/sample/Polarsys.java
@@ -14,8 +14,6 @@ import org.json.simple.JSONObject;
 public class Polarsys {
 
   public static void main(String args[]) {
-    // Use Eclipse SONAR: https://dev.eclipse.org/sonar/api/metrics?resource=org.eclipse.mylyn.tasks:org.eclipse.mylyn.tasks-parent
-    // https://dev.eclipse.org/sonar/dashboard/index/org.eclipse.cdt:cdt-parent?did=1
     String url = "https://dev.eclipse.org/sonar";
     String login = "admin";
     String password = "admin";
@@ -25,28 +23,28 @@ public class Polarsys {
     // projectKey = "org.eclipse.emf.compare.features:org.eclipse.emf.compare.diagram.papyrus";
     // projectKey = "org.eclipse.emf.compare:org.eclipse.emf.compare.diagram.papyrus.tests";
     // projectKey = "org.eclipse.emf.compare:org.eclipse.emf.compare.diagram.ide.ui.papyrus";
-    projectKey = "org.eclipse.ease:ease";
+    // projectKey = "org.eclipse.ease:ease";
     // projectKey = "org.eclipse.ease:ease.modules:EASE.modules";
 	System.out.println(projectKey);
 
+    // dit metrics is not available in SONAR CDT project
     ResourceQuery query = ResourceQuery.createForMetrics(projectKey,
     		"line_coverage","tests","test_success_density","ncloc",
     		"functions","complexity","comment_lines_density",
-    		"uncovered_lines","duplicated_lines","weighted_violations");
+    		"uncovered_lines","duplicated_lines","weighted_violations","public_api","dit");
     // query.setIncludeTrends(true);
-    Resource mylyn = sonar.find(query);
+    Resource metrics = sonar.find(query);
 
     JSONObject obj = new JSONObject();
 
     //getVariation2 for "7 days"
-    List<Measure> allMeasures = mylyn.getMeasures();
+    List<Measure> allMeasures = metrics.getMeasures();
     for (Measure measure : allMeasures) {
         System.out.println(measure.getMetricKey()+": "+measure.getValue());
     	obj.put(measure.getMetricKey(), measure.getValue());
     }
 
     try {
-		// FileWriter file = new FileWriter("eclipse_sonar.json");
 		FileWriter file = new FileWriter(projectKey + ".json");
 		file.write(obj.toJSONString());
 		file.flush();
